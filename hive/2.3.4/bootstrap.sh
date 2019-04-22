99e465c1fd44ba8cb3d3eb0b979f00f5e0f8ba4e
袁徐磊(Yuan Xulei) <hi@yxl.name>
2019-04-22 11:09:28 +0800

    update hive to 2.3.4 and fix hadoop custom configuration files issue

--- hive/2.3.4/bootstrap.sh
+++ hive/2.3.4/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-## replace config
+## replace hadoop config
+HADOOP_EXTRA_CONF_DIR = /config/hadoop
+if [ -d "$HADOOP_EXTRA_CONF_DIR" ]; then
+	cp $HADOOP_EXTRA_CONF_DIR/* /usr/local/hadoop/etc/hadoop/
+fi
+
+## replace hive config
 : ${EXTRA_CONF_DIR:=/config/hive}
 
 if [ -d "$EXTRA_CONF_DIR" ]; then
