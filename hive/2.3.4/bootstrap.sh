807e6508a61cda789a75ada48afae7640655703d
袁徐磊(Yuan Xulei) <hi@yxl.name>
2019-05-10 16:17:01 +0800

    fix bootstrap

--- hive/2.3.4/bootstrap.sh
+++ hive/2.3.4/bootstrap.sh
@@ -18,7 +18,7 @@ $HADOOP_HOME/bin/hdfs dfs -mkdir /tmp
 $HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/warehouse
 $HADOOP_HOME/bin/hdfs dfs -chmod g+w /tmp
 $HADOOP_HOME/bin/hdfs dfs -chmod g+w /user/hive/warehouse
-schematool --dbType mysql --initSchem
+schematool --dbType mysql --initSchema
 
 nohup hive --service metastore &
 hiveserver2
